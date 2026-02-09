bffd34235144e1d18ceb477509426c3bf4126933
navya01106 <navyaagarwal01106@gmail.com>
2026-02-09 17:10:46 +0000

    day9

---
+++ day9/q18.c
@@ -0,0 +1,30 @@
+/*Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+*/
+void reverseString(char* s, int sSize) {
+   int left = 0;
+    int right = sSize - 1;
+    
+    while (left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
+ int main() {
+    char s[] = {'h', 'e', 'l', 'l', 'o'};
+    int size = 5;
+    
+    reverseString(s, size);
+    
+    for (int i = 0; i < size; i++) {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
