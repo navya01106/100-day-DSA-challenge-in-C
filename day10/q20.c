98532ee612a6f656a1ebb75d4b980dcad8ed3e72
navya01106 <navyaagarwal01106@gmail.com>
2026-02-10 09:49:17 +0000

    day10

---
+++ day10/q20.c
@@ -0,0 +1,19 @@
+//Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+#include<stdio.h>
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    int freq[1001] = {0};
+    for (int i = 0; i < nums1Size; i++) {
+        freq[nums1[i]]++;
+    }
+    int* result = (int*)malloc(sizeof(int) * (nums1Size < nums2Size ? nums1Size : nums2Size));
+    int k = 0;
+    for (int i = 0; i < nums2Size; i++) {
+        if (freq[nums2[i]] > 0) {
+            result[k++] = nums2[i];
+            freq[nums2[i]]--;
+        }
+    }
+
+    *returnSize = k;
+    return result;
+}
