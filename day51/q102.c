41773873b85655f19000c403fa512a166a127f94
navya01106 <navyaagarwal01106@gmail.com>
2026-03-27 17:29:49 +0000

    day51

---
+++ day51/q102.c
@@ -0,0 +1,8 @@
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (!root) return NULL;
+    if (p->val < root->val && q->val < root->val)
+        return lowestCommonAncestor(root->left, p, q);
+    if (p->val > root->val && q->val > root->val)
+        return lowestCommonAncestor(root->right, p, q);
+    return root;
+}
