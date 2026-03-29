4521dc46c297a1dfb98067ffd0614e77cda5f254
navya01106 <navyaagarwal01106@gmail.com>
2026-03-29 18:16:12 +0000

    day56

---
+++ day56/q112.c
@@ -0,0 +1,12 @@
+bool isMirror(struct TreeNode* left, struct TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    return (left->val == right->val)
+        && isMirror(left->left, right->right)
+        && isMirror(left->right, right->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (!root) return true;
+    return isMirror(root->left, root->right);
+}
