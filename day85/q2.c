b6162866940f10e2b6f81bfaaa8a88ebedbb5a82
navya01106 <navyaagarwal01106@gmail.com>
2026-04-27 18:37:16 +0000

    day85

---
+++ day85/q2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int findMin(int nums[], int n) {
+    int low = 0, high = n - 1;
+
+    while (low < high) {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] > nums[high]) {
+            low = mid + 1;   // minimum in right half
+        } else {
+            high = mid;      // minimum in left half (including mid)
+        }
+    }
+
+    return nums[low];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    printf("%d", findMin(nums, n));
+
+    return 0;
+}
